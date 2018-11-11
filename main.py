d56be759091e2b554578386595f3474337dc7aa4
Clint Worrell <github@clintworrell.com>
2018-11-10 22:00:13 -0800

    Move some logic to functions

--- main.py
+++ main.py
@@ -5,15 +5,38 @@ import pprint
 pp = pprint.PrettyPrinter()
 
 client = boto3.client('ec2')
-response = client.describe_vpcs()
 
-vpcs = response['Vpcs']
-if vpcs:
+vpcs = client.describe_vpcs()['Vpcs']
+
+
+def detect_vpc(ip):
     for vpc in vpcs:
-        if 'Tags' in vpc:
-            pp.pprint(vpc['CidrBlock'] + ' > ' + vpc['Tags'][0]['Value'])
+        vpc_cidr = ipaddress.IPv4Network(vpc['CidrBlock'])
+        vpc_name = get_vpc_name_tag(vpc)
+        ip = ipaddress.IPv4Address(ip)
+
+        if ip in vpc_cidr:
+            print(f"{str(ip)} is in {str(vpc_cidr)} ({vpc_name})")
+            break
         else:
-            pp.pprint(vpc['CidrBlock'])
-        print(ipaddress.IPv4Address('10.20.48.231') in ipaddress.IPv4Network(vpc['CidrBlock']))
-else:
-    print('No VPCs found')
+            print(f"{str(ip)} is not in {str(vpc_cidr)}")
+
+
+def get_vpc_name_tag(vpc):
+    try:
+        vpc['Tags'][0]['Value']
+    except KeyError:
+        present = False
+    else:
+        present = True
+
+    if present:
+        vpc_name = vpc['Tags'][0]['Value']
+    else:
+        vpc_name = "VPC name tag missing"
+
+    return vpc_name
+
+
+detect_vpc("10.20.48.231")
+detect_vpc("172.31.25.149")
