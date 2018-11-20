9c253d0c49b91b36b1f46e46cf47480adb2eaa03
Clint Worrell <github@clintworrell.com>
2018-11-19 16:31:47 -0800

    Find source security group and associated NACL

--- main.py
+++ main.py
@@ -12,25 +12,21 @@ print(f"Checking if {args.source_ip} and {args.destination_ip} are in VPCs...")
 
 pp = pprint.PrettyPrinter()
 
-client = boto3.client('ec2')
+ec2 = boto3.client('ec2')
 
-source_ip = args.source_ip
-destination_ip = args.destination_ip
-vpcs = client.describe_vpcs()['Vpcs']
+vpc_resp = ec2.describe_vpcs()
 
-
-def detect_vpc(ip):
+def get_vpc(ip):
+    vpcs = vpc_resp['Vpcs']
     for vpc in vpcs:
         vpc_cidr = ipaddress.IPv4Network(vpc['CidrBlock'])
         vpc_name = get_vpc_name_tag(vpc)
-        ip = ipaddress.IPv4Address(ip)
+        vpc_id = vpc['VpcId']
 
         if ip in vpc_cidr:
-            print(f"{str(ip)} is in {str(vpc_cidr)} ({vpc_name})")
-            break
+            return vpc_id
         else:
-            print(f"{str(ip)} is not in {str(vpc_cidr)}")
-
+            return None
 
 def get_vpc_name_tag(vpc):
     try:
@@ -48,5 +44,82 @@ def get_vpc_name_tag(vpc):
     return vpc_name
 
 
-detect_vpc(source_ip)
-detect_vpc(destination_ip)
+def get_vpc_subnets(vpc_id):
+    subnets = ec2.describe_subnets(
+        Filters = [
+            {
+                'Name': 'vpc-id',
+                'Values': [
+                    vpc_id,
+                ]
+            },
+        ]
+    )
+
+    # subnets = subnets['Subnets']
+
+    return subnets
+
+
+def main():
+    source_ip = ipaddress.IPv4Address(args.source_ip)
+    destination_ip = ipaddress.IPv4Address(args.destination_ip)
+
+    source_vpc_id = get_vpc(source_ip)
+    destination_vpc_id = get_vpc(destination_ip)
+
+    if source_vpc_id and destination_vpc_id:
+        print("Source and destination are in VPCs...")
+        source_vpc_subnets = get_vpc_subnets(source_vpc_id)
+        print(source_vpc_subnets)
+        destination_vpc_subnets = get_vpc_subnets(destination_vpc_id)
+
+    for subnet in source_vpc_subnets['Subnets']:
+        subnet_cidr = ipaddress.IPv4Network(subnet['CidrBlock'])
+        if source_ip in subnet_cidr:
+            source_subnet = subnet
+            print(source_subnet)
+            break
+        else:
+            source_subnet = None
+            print("Source IP isn't in a subnet")
+
+    for subnet in destination_vpc_subnets['Subnets']:
+        subnet_cidr = ipaddress.IPv4Network(subnet['CidrBlock'])
+        if destination_ip in subnet_cidr:
+            destination_subnet = subnet
+            print(destination_subnet)
+            break
+        else:
+            destination_subnet = None
+            print("Destination IP isn't in a subnet")
+
+    if source_subnet and destination_subnet:
+        if source_subnet['VpcId'] != destination_subnet['VpcId']:
+            # Check peering
+            # Check routes
+            # Check NACLs (inbound and outbound on both source and destination)
+            # Check security groups
+            pass
+        else:
+            # Source and destination are in the same VPC
+            # Check that source_subnet NACL has egress rule to destination subnet
+            source_subnet_id = source_subnet['SubnetId']
+            source_subnet_nacl = ec2.describe_network_acls(
+                Filters = [
+                    {
+                        'Name': 'association.subnet-id',
+                        'Values': [
+                            source_subnet_id,
+                        ]
+                    }
+                ]
+            )
+            print(source_subnet_nacl)
+            print(source_subnet_id)
+            # Check that NACLs allow traffic to itself
+            # Check subnets allow egress from source to dest and allow ingress from source to dest
+            pass
+
+if __name__ == "__main__":
+    main()
