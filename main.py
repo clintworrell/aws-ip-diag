e9992266c283e3d55a0c1b78d8fe6511a886077b
Clint Worrell <github@clintworrell.com>
2018-11-20 09:50:47 -0800

    Clean up some blocks

--- main.py
+++ main.py
@@ -3,24 +3,21 @@ import boto3
 import ipaddress
 import pprint
 
-# Testing out argparse
+# argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("source_ip", help="Source IP of the traffic")
 parser.add_argument("destination_ip", help="Destination IP of the traffic")
 args = parser.parse_args()
 print(f"Checking if {args.source_ip} and {args.destination_ip} are in VPCs...")
 
-pp = pprint.PrettyPrinter()
-
 ec2 = boto3.client('ec2')
 
-vpc_resp = ec2.describe_vpcs()
-
+# Determine if IP is part of a VPC CIDR
 def get_vpc(ip):
-    vpcs = vpc_resp['Vpcs']
+    vpcs = ec2.describe_vpcs()['Vpcs']
     for vpc in vpcs:
         vpc_cidr = ipaddress.IPv4Network(vpc['CidrBlock'])
-        vpc_name = get_vpc_name_tag(vpc)
+        vpc_name = get_vpc_name(vpc)
         vpc_id = vpc['VpcId']
 
         if ip in vpc_cidr:
@@ -28,7 +25,8 @@ def get_vpc(ip):
         else:
             return None
 
-def get_vpc_name_tag(vpc):
+
+def get_vpc_name(vpc):
     try:
         vpc['Tags'][0]['Value']
     except KeyError:
@@ -64,16 +62,21 @@ def get_vpc_subnets(vpc_id):
 def main():
     source_ip = ipaddress.IPv4Address(args.source_ip)
     destination_ip = ipaddress.IPv4Address(args.destination_ip)
+    ips = [source_ip, destination_ip]
 
     source_vpc_id = get_vpc(source_ip)
     destination_vpc_id = get_vpc(destination_ip)
+    vpc_ids = [source_vpc_id, destination_vpc_id]
 
     if source_vpc_id and destination_vpc_id:
         print("Source and destination are in VPCs...")
         source_vpc_subnets = get_vpc_subnets(source_vpc_id)
         print(source_vpc_subnets)
         destination_vpc_subnets = get_vpc_subnets(destination_vpc_id)
+        vpc_subnets = [source_vpc_subnets, destination_vpc_subnets]
 
+    # Identify source and destination subnets
+    # Should be refactored later
     for subnet in source_vpc_subnets['Subnets']:
         subnet_cidr = ipaddress.IPv4Network(subnet['CidrBlock'])
         if source_ip in subnet_cidr:
@@ -94,6 +97,7 @@ def main():
             destination_subnet = None
             print("Destination IP isn't in a subnet")
 
+    # Identify NACLs associated with the source and destination subnets
     if source_subnet and destination_subnet:
         if source_subnet['VpcId'] != destination_subnet['VpcId']:
             # Check peering
