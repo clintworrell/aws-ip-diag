0645a851e8ee29629c29ecb21050f68e2b2243e2
Clint Worrell <github@clintworrell.com>
2018-11-22 17:51:54 -0800

    Refactor getting subnets and NACLS

--- main.py
+++ main.py
@@ -62,82 +62,64 @@ def get_vpc_subnets(vpc_id):
     return subnets
 
 
+def get_subnet(vpc_subnets, ip):
+    for vpc_subnet in vpc_subnets['Subnets']:
+        subnet_cidr = ipaddress.IPv4Network(vpc_subnet['CidrBlock'])
+
+        if ip in subnet_cidr:
+            subnet = vpc_subnet
+            break
+
+    return subnet
+
+
+def get_nacl(subnet):
+    subnet_id = subnet['SubnetId']
+    nacl = ec2.describe_network_acls(
+        Filters = [
+            {
+                'Name': 'association.subnet-id',
+                'Values': [
+                    subnet_id,
+                ]
+            }
+        ]
+    )
+
+    return nacl
+
+
 def main():
     source_ip = ipaddress.IPv4Address(args.source_ip)
     destination_ip = ipaddress.IPv4Address(args.destination_ip)
-    ips = [source_ip, destination_ip]
 
     source_vpc_id = get_vpc(source_ip)
     destination_vpc_id = get_vpc(destination_ip)
-    vpc_ids = [source_vpc_id, destination_vpc_id]
 
+    # Get the subnets of the VPCs
     if source_vpc_id and destination_vpc_id:
         print("Source and destination are in VPCs...")
         source_vpc_subnets = get_vpc_subnets(source_vpc_id)
-        # print(source_vpc_subnets)
         destination_vpc_subnets = get_vpc_subnets(destination_vpc_id)
         vpc_subnets = [source_vpc_subnets, destination_vpc_subnets]
     else:
+        print("Source and/or destination are not in a VPC")
         return
 
-    # Identify source and destination subnets
-    # Should be refactored later
-    source_subnet = None
-    for subnet in source_vpc_subnets['Subnets']:
-        subnet_cidr = ipaddress.IPv4Network(subnet['CidrBlock'])
+    # Get the source and destination subnets
+    source_subnet = get_subnet(source_vpc_subnets, source_ip)
+    destination_subnet = get_subnet(destination_vpc_subnets, destination_ip)
 
-        if source_ip in subnet_cidr:
-            source_subnet = subnet
-            break
-
-    if source_subnet == None:
-        print("Source IP isn't in a subnet")
-        return
-
-    destination_subnet = None
-    for subnet in destination_vpc_subnets['Subnets']:
-        subnet_cidr = ipaddress.IPv4Network(subnet['CidrBlock'])
-        if destination_ip in subnet_cidr:
-            destination_subnet = subnet
-            # print(destination_subnet)
-            break
-    if destination_subnet == None:
-        print("Destination IP isn't in a subnet")
-        return
-
-    # Identify NACLs associated with the source and destination subnets
+    # Get the source and destination NACLs
     if source_subnet and destination_subnet:
-        source_subnet_id = source_subnet['SubnetId']
-        source_subnet_nacl = ec2.describe_network_acls(
-            Filters = [
-                {
-                    'Name': 'association.subnet-id',
-                    'Values': [
-                        source_subnet_id,
-                    ]
-                }
-            ]
-        )
-
-        destination_subnet_id = destination_subnet['SubnetId']
-        destination_subnet_nacl = ec2.describe_network_acls(
-            Filters = [
-                {
-                    'Name': 'association.subnet-id',
-                    'Values': [
-                        destination_subnet_id,
-                    ]
-                }
-            ]
-        )
-
-        # print(source_subnet_nacl)
+        source_nacl = get_nacl(source_subnet)
+        destination_nacl = get_nacl(destination_subnet)
 
         # Get NACL entries
-        source_nacl_entries = source_subnet_nacl['NetworkAcls'][0]['Entries']
+        source_nacl_entries = source_nacl['NetworkAcls'][0]['Entries']
         source_nacl_entries = sorted(source_nacl_entries, key=lambda x: x['RuleNumber'])
 
-        destination_nacl_entries = destination_subnet_nacl['NetworkAcls'][0]['Entries']
+        destination_nacl_entries = destination_nacl['NetworkAcls'][0]['Entries']
         destination_nacl_entries = sorted(destination_nacl_entries, key=lambda x: x['RuleNumber'])
 
     if source_subnet['VpcId'] == destination_subnet['VpcId']:
